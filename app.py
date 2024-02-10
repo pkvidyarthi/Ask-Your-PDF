f9b32028f1525268989eefc347034cf4ea8f2e46
Prince Kumar <72149077+pkvidyarthi@users.noreply.github.com>
2024-02-10 21:44:39 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from tiktoken._educational import *
 
 def main():
