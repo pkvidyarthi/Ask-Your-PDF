729af592ba6141494fea0e1c84c5dd90aa558e19
Prince Kumar <72149077+pkvidyarthi@users.noreply.github.com>
2024-02-10 19:51:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
+from tiktoken._educational import *
 
 def main():
     load_dotenv()
