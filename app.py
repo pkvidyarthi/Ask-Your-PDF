c55aaeac50ee646e7e179925e3ffa288aa4c07da
Prince Kumar <72149077+pkvidyarthi@users.noreply.github.com>
2025-09-22 00:03:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,54 @@
-# Importing Modules
-
 from dotenv import load_dotenv
 import os
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, OpenAI
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain_community.llms import OpenAI
-from tiktoken._educational import *
+
+# Load environment variables
+load_dotenv()
 
 def main():
-    load_dotenv()
     st.set_page_config(page_title="Ask Your PDF")
-    # Display the header with a specific text color
-    st.markdown(f'<h1 style="color: #D2042D;">Ask Your PDF</h1>', unsafe_allow_html=True)
+
+    # Header
+    st.markdown('<h1 style="color: #D2042D;">Ask Your PDF</h1>', unsafe_allow_html=True)
     st.write("Discover answers within your uploaded PDF effortlessly.\nUpload your PDF and uncover answers tailored to its content with ease.")
 
     # Upload File
-    pdf = st.file_uploader("Upload Your PDF", type = "pdf")
+    pdf = st.file_uploader("Upload Your PDF", type="pdf")
 
-    # Extract The Text
     if pdf is not None:
+        # Extract text
         pdf_reader = PdfReader(pdf)
         text = ""
         for page in pdf_reader.pages:
-            text += page.extract_text()
+            text += page.extract_text() or ""
 
-        # Split into Chunks
+        # Split text into chunks
         text_splitter = CharacterTextSplitter(
-            separator = "\n",
-            chunk_size = 1000,
-            chunk_overlap = 200,
-            length_function = len
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
         )
         chunks = text_splitter.split_text(text)
 
-        # Create Embeddings
-        embeddings = OpenAIEmbeddings()
+        # Create embeddings
+        embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
         knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-        # Show User Input
+        # User Input
         user_question = st.text_input("Ask a query about your PDF : ")
         if user_question:
             docs = knowledge_base.similarity_search(user_question)
-            llm = OpenAI()
-            chain = load_qa_chain(llm, chain_type = "stuff")
-            response = chain.run(input_documents = docs, question = user_question)
+            llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+            chain = load_qa_chain(llm, chain_type="stuff")
+            response = chain.run(input_documents=docs, question=user_question)
             st.write(response)
 
-if _name_ == "_main_":
-    # st.run(main, debug=True)
+
+if __name__ == "__main__":
     main()
