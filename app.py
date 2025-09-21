3bb9d07a8b4e12b8bf9a977121b550c441919c37
Prince Kumar <72149077+pkvidyarthi@users.noreply.github.com>
2025-09-22 00:07:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, OpenAI
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 
 # Load environment variables
